cfbcb32bd521ceb9ecc72f06caf5f9738a2c686d
giordanofabrizii <giordanofabrizi@gmail.com>
2024-09-04 15:49:20 +0200

    changed data types

--- database/migrations/2024_09_04_133728_create_visualizations_table.php
+++ database/migrations/2024_09_04_133728_create_visualizations_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sponsorships', function (Blueprint $table) {
+        Schema::create('visualizations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->tinyInteger('period');
-            $table->decimal('price', 6, 2);
+            $table->foreignId('apartment_id')->constrained()->onDelete('cascade');
+            $table->date('date');
+            $table->string('user_ip');
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sponsorships');
+        Schema::dropIfExists('visualizations');
     }
 };
