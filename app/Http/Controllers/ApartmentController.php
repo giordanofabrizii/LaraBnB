169928d56b86a20b415805dd1334452cb224c4de
giordanofabrizii <giordanofabrizi@gmail.com>
2024-09-06 11:26:50 +0200

    added show

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -22,6 +22,10 @@ class ApartmentController extends Controller
         return view('apartments.index', compact('apartments'));
     }
 
+    public function show (Apartment $apartment) {
+        return view('apartments.show',compact('apartment'));
+    }
+
     /**
      * create a new apartment
      *
