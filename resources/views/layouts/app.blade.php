b86e49d754cc546b482f3e25c5b5cc5b0fcdafa2
giordanofabrizii <giordanofabrizi@gmail.com>
2024-09-06 23:23:41 +0200

    added dark mode switcher

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -63,6 +63,8 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <button class="dropdown-item" href="" id="dark-mode">Dark Mode</button>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                         onclick="event.preventDefault();
                                             document.getElementById('logout-form').submit();">
