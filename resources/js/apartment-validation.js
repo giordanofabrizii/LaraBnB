d04a9b6a4529e0fcb390ccae3d4ba0a238f05cee
giordanofabrizii <giordanofabrizi@gmail.com>
2024-09-10 19:53:34 +0200

    fixed validation for the create

--- resources/js/apartment-validation.js
+++ resources/js/apartment-validation.js
@@ -1,123 +1,11 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const myForm = document.getElementById('formEl');
 
-    // ! NAME string length > 2
-    const nameEl = document.getElementById('name');
-    const nameErrorEl = document.querySelector('#name ~ .error');
-    nameEl.addEventListener('input', () => {
-        const nameLength = nameEl.value;
-        console.log("input")
-        if (nameLength.trim().length < 3) {
-            nameErrorEl.classList.add('on');
-            nameErrorEl.innerText = "Inserisci un nome di almeno 3 lettere";
-        } else {
-            nameErrorEl.classList.remove('on');
-            nameErrorEl.innerText = "";
-        }
-    })
-
-    // ! DESCRIPTION string length > 9
-    const descriptionEl = document.getElementById('description');
-    const descriptionErrorEl = document.querySelector('#description + .error');
-    descriptionEl.addEventListener('input', function() {
-        const descriptionLength = descriptionEl.value.length;
-        if (descriptionLength < 10) {
-            descriptionErrorEl.classList.add('on');
-            descriptionErrorEl.innerText = "Inserisci un nome di almeno 9 lettere";
-        } else {
-            descriptionErrorEl.classList.remove('on');
-            descriptionErrorEl.innerText = "";
-        }
-    })
-
-    // ! ADDRESS string length > 0
-    const addressEl = document.getElementById('address');
-    const addressErrorEl = document.querySelector('[name="address"] + .error');
-    addressEl.addEventListener('input', function() {
-        const addressLength = addressEl.value.length;
-        if (addressLength < 1) {
-            addressErrorEl.classList.add('on');
-            addressErrorEl.innerText = "Inserisci un indirizzo";
-        } else {
-            addressErrorEl.classList.remove('on');
-            addressErrorEl.innerText = "";
-        }
-    })
-
-    // ! SURFACE number > 0
-    const surfaceEl = document.getElementById('surface');
-    const surfaceErrorEl = document.querySelector('[name="surface"] + .error');
-    surfaceEl.addEventListener('input', function() {
-        console.log('surface')
-        const surfaceText = surfaceEl.value;
-        if (surfaceText.length < 0 || isNaN(Number(surfaceText)) || Number(surfaceText) <= 0) {
-            surfaceErrorEl.classList.add('on');
-            surfaceErrorEl.innerText = "Inserisci un numero positivo";
-        } else {
-            surfaceErrorEl.classList.remove('on');
-            surfaceErrorEl.innerText = "";
-        }
-    })
-
-    // ! ROOM
-    const n_roomEl = document.getElementById('n_room');
-    const n_roomErrorEl = document.querySelector('[name="n_room"] + .error');
-    n_roomEl.addEventListener('input', function() {
-        const roomText = n_roomEl.value;
-        if (roomText.length < 0 ||isNaN(Number(roomText)) || Number(roomText) <= 0) {
-            n_roomErrorEl.classList.add('on');
-            n_roomErrorEl.innerText = "Inserisci un numero positivo";
-        } else {
-            n_roomErrorEl.classList.remove('on');
-            n_roomErrorEl.innerText = "";
-        }
-    })
-
-    // ! BED
-    const n_bedEl = document.getElementById('n_bed');
-    const n_bedErrorEl = document.querySelector('[name="n_bed"] + .error');
-    n_bedEl.addEventListener('input', function() {
-        const bedText = n_bedEl.value;
-        if (bedText.length < 0 ||isNaN(Number(bedText)) || Number(bedText) <= 0) {
-            n_bedErrorEl.classList.add('on');
-            n_bedErrorEl.innerText = "Inserisci un numero positivo";
-        } else {
-            n_bedErrorEl.classList.remove('on');
-            n_bedErrorEl.innerText = "";
-        }
-    })
-
-    // ! BATH
-    const n_bathEl = document.getElementById('n_bath');
-    const n_bathErrorEl = document.querySelector('[name="n_bath"] + .error');
-    n_bathEl.addEventListener('input', function() {
-    const bathText = n_bathEl.value;
-        if (bathText.length < 0 ||isNaN(Number(bathText)) || Number(bathText) <= 0) {
-            n_bathErrorEl.classList.add('on');
-            n_bathErrorEl.innerText = "Inserisci un numero positivo";
-        } else {
-            n_bathErrorEl.classList.remove('on');
-            n_bathErrorEl.innerText = "";
-        }
-    })
-
-    // ! PRICE
-    const priceEl = document.getElementById('price');
-    const priceErrorEl = document.querySelector('[name="price"] + .error');
-    priceEl.addEventListener('input', function() {
-        const priceText = priceEl.value;
-        if (priceText.length < 0 ||isNaN(Number(priceText)) || Number(priceText) <= 0) {
-            priceErrorEl.classList.add('on');
-            priceErrorEl.innerText = "Inserisci un numero positivo";
-        } else {
-            priceErrorEl.classList.remove('on');
-            priceErrorEl.innerText = "";
-        }
-    })
-
-
     myForm.addEventListener('submit', function (event) {
         event.preventDefault();
+
+        updateInput();
 
         // Inizializza una variabile per tenere traccia se ci sono errori
         let result = validate();
@@ -132,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function validate() {
     const inputs = document.querySelectorAll('.error');
+    console.log(inputs)
 
     for (let i = 0; i < inputs.length; i++) {
         if (inputs[i].classList.contains('on')) { // if an input is showed, return false
@@ -139,4 +28,173 @@ function validate() {
         }
     }
     return true; // else return true
+}
+
+function updateInput() {
+    // ! NAME string length > 2
+    const nameEl = document.getElementById('name'); // take the input element
+    const nameErrorEl = document.querySelector('#name ~ .error'); // take the error div
+
+    /**
+     * Add a class "on" if the input is not validated
+     *
+     * @param {*} nameEl
+     * @param {*} nameErrorEl
+     */
+    function nameFunc(nameEl, nameErrorEl) {
+        const nameLength = nameEl.value;
+        if (nameLength.trim().length < 3) {
+            nameErrorEl.classList.add('on');
+            nameErrorEl.innerText = "Inserisci un nome di almeno 3 lettere";
+        } else {
+            nameErrorEl.classList.remove('on');
+            nameErrorEl.innerText = "";
+        }
+    }
+
+    // call the function
+    nameFunc(nameEl, nameErrorEl);
+    nameEl.addEventListener('input', () => { // when the input is uploaded, refresh the error
+        nameFunc(nameEl, nameErrorEl)
+    })
+
+    // ! DESCRIPTION string length > 9
+    const descriptionEl = document.getElementById('description');
+    const descriptionErrorEl = document.querySelector('#description ~ .error');
+
+    function descriptionFunc(descriptionEl,descriptionErrorEl){
+        const descriptionLength = descriptionEl.value.length;
+        if (descriptionLength < 10) {
+            descriptionErrorEl.classList.add('on');
+            descriptionErrorEl.innerText = "Inserisci un nome di almeno 9 lettere";
+        } else {
+            descriptionErrorEl.classList.remove('on');
+            descriptionErrorEl.innerText = "";
+        }
+    }
+
+    descriptionFunc(descriptionEl,descriptionErrorEl);
+    descriptionEl.addEventListener('input', function() {
+        descriptionFunc(descriptionEl,descriptionErrorEl);
+    })
+
+    // ! ADDRESS string length > 0
+    const addressEl = document.getElementById('address');
+    const addressErrorEl = document.querySelector('[name="address"] ~ .error');
+
+    function addressFunc(addressEl, addressErrorEl){
+        const addressLength = addressEl.value.length;
+        if (addressLength < 1) {
+            addressErrorEl.classList.add('on');
+            addressErrorEl.innerText = "Inserisci un indirizzo";
+        } else {
+            addressErrorEl.classList.remove('on');
+            addressErrorEl.innerText = "";
+        }
+    }
+
+    addressFunc(addressEl, addressErrorEl);
+    addressEl.addEventListener('input', function() {
+        addressFunc(addressEl, addressErrorEl);
+    })
+
+    // ! SURFACE number > 0
+    const surfaceEl = document.getElementById('surface');
+    const surfaceErrorEl = document.querySelector('[name="surface"] ~ .error');
+
+    function surfaceFunc(surfaceEl,surfaceErrorEl){
+        const surfaceText = surfaceEl.value;
+        if (surfaceText.length < 0 || isNaN(Number(surfaceText)) || Number(surfaceText) <= 0) {
+            surfaceErrorEl.classList.add('on');
+            surfaceErrorEl.innerText = "Inserisci un numero positivo";
+        } else {
+            surfaceErrorEl.classList.remove('on');
+            surfaceErrorEl.innerText = "";
+        }
+    }
+
+    surfaceFunc(surfaceEl,surfaceErrorEl);
+    surfaceEl.addEventListener('input', function() {
+        surfaceFunc(surfaceEl,surfaceErrorEl);
+    })
+
+    // ! ROOM
+    const n_roomEl = document.getElementById('n_room');
+    const n_roomErrorEl = document.querySelector('[name="n_room"] ~ .error');
+
+    function roomFunc(n_roomEl, n_roomErrorEl){
+        const roomText = n_roomEl.value;
+        if (roomText.length < 0 ||isNaN(Number(roomText)) || Number(roomText) <= 0) {
+            n_roomErrorEl.classList.add('on');
+            n_roomErrorEl.innerText = "Inserisci un numero positivo";
+        } else {
+            n_roomErrorEl.classList.remove('on');
+            n_roomErrorEl.innerText = "";
+        }
+    }
+
+    roomFunc(n_roomEl, n_roomErrorEl);
+    n_roomEl.addEventListener('input', function() {
+        roomFunc(n_roomEl, n_roomErrorEl);
+    })
+
+    // ! BED
+    const n_bedEl = document.getElementById('n_bed');
+    const n_bedErrorEl = document.querySelector('[name="n_bed"] ~ .error');
+
+    function bedFunc(n_bedEl,n_bedErrorEl){
+        const bedText = n_bedEl.value;
+        if (bedText.length < 0 ||isNaN(Number(bedText)) || Number(bedText) <= 0) {
+            n_bedErrorEl.classList.add('on');
+            n_bedErrorEl.innerText = "Inserisci un numero positivo";
+        } else {
+            n_bedErrorEl.classList.remove('on');
+            n_bedErrorEl.innerText = "";
+        }
+    }
+
+    bedFunc(n_bedEl,n_bedErrorEl);
+    n_bedEl.addEventListener('input', function() {
+        bedFunc(n_bedEl,n_bedErrorEl);
+    })
+
+    // ! BATH
+    const n_bathEl = document.getElementById('n_bath');
+    const n_bathErrorEl = document.querySelector('[name="n_bath"] ~ .error');
+
+    function bathFunc(n_bathEl,n_bathErrorEl){
+        const bathText = n_bathEl.value;
+        if (bathText.length < 0 ||isNaN(Number(bathText)) || Number(bathText) <= 0) {
+            n_bathErrorEl.classList.add('on');
+            n_bathErrorEl.innerText = "Inserisci un numero positivo";
+        } else {
+            n_bathErrorEl.classList.remove('on');
+            n_bathErrorEl.innerText = "";
+        }
+    }
+
+    bathFunc(n_bathEl ,n_bathErrorEl);
+    n_bathEl.addEventListener('input', function() {
+        bathFunc(n_bathEl ,n_bathErrorEl);
+    })
+
+    // ! PRICE
+    const priceEl = document.getElementById('price');
+    const priceErrorEl = document.querySelector('[name="price"] ~ .error');
+
+    function priceFunc(priceEl, priceErrorEl){
+        const priceText = priceEl.value;
+        if (priceText.length < 0 ||isNaN(Number(priceText)) || Number(priceText) <= 0) {
+            priceErrorEl.classList.add('on');
+            priceErrorEl.innerText = "Inserisci un numero positivo";
+        } else {
+            priceErrorEl.classList.remove('on');
+            priceErrorEl.innerText = "";
+        }
+    }
+
+    priceFunc(priceEl, priceErrorEl);
+    priceEl.addEventListener('input', function() {
+        priceFunc(priceEl, priceErrorEl);
+    })
 }
--- resources/views/apartments/create.blade.php
+++ resources/views/apartments/create.blade.php
@@ -10,26 +10,25 @@
         <div class="col-12 p-3">
             <h1>Add a new apartment</h1>
         </div>
-
         <div class="col-10">
-            <form action="{{ route('apartments.store') }}" method="POST" enctype="multipart/form-data">
+            <form id="formEl" action="{{ route('apartments.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
 
                 <div class="form-group p-2">
                     <label class="mb-2" for="name">Apartment's name:</label>
                     <input type="text" class="form-control" name="name" id="name" placeholder="Type a name for the apartment" value="{{ old('name') }}">
+                    <div class="error"></div>
                     @error('name')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
                 </div>
                 <div class="form-group p-2">
                     <label class="mb-2" for="description">Description:</label>
                     <input type="text" class="form-control" name="description" id="description" placeholder="Add a description" value="{{ old('description') }}">
+                    <div class="error"></div>
                     @error('description')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
                 </div>
                 <div class="form-group p-2">
                     <div class="form-group p-2">
@@ -39,49 +38,49 @@
                         <div id="map" style="width:70%; height: 290px;" class="m-auto"></div>
                         <input type="hidden" id="latitude" name="latitude">
                         <input type="hidden" id="longitude" name="longitude">
+                        <div class="error"></div>
 
                         @error('description')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
-                        <div class="error"></div>
                 </div>
                 <div class="form-group p-2">
                     <label class="mb-2" for="surface">Surface (in m2):</label>
                     <input type="number" class="form-control" name="surface" id="surface" placeholder="How many m2" value="{{ old('surface') }}">
+                    <div class="error"></div>
                     @error('surface')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
                 </div>
                 <div class="form-group p-2">
                     <label for="image">Want to edit the image?</label>
                     <input type="file" class="form-control" name="image" id="image" placeholder="Upload an image">
+                    <div class="error"></div>
                     @error('image')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
                 </div>
                 <div class="form-group p-2">
                     <label class="m-2" for="n_room">How many rooms?</label>
                     <input type="number" name="n_room" id="n_room" value="{{ old('n_room') }}">
+                    <div class="error"></div>
                     @error('n_room')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
 
                     <label class="m-2" for="n_bed">How many beds?</label>
                     <input type="number" name="n_bed" id="n_bed" value="{{ old('n_bed') }}">
+                    <div class="error"></div>
                     @error('n_bed')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
 
                     <label class="m-2" for="n_bath">How many bathrooms?</label>
                     <input type="number" name="n_bath" id="n_bath" value="{{ old('n_bath') }}">
+                    <div class="error"></div>
                     @error('n_bath')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
                 </div>
                 <div class="form-group">
                     <label for="service_id">Services</label>
@@ -95,12 +94,12 @@
                 <div class="form-group p-2">
                     <label class="m-2" for="price">Price per night:</label>
                     <input type="price" name="price" id="price" value="{{ old('price') }}">
+                    <div class="error"></div>
                     @error('price')
                         <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
-                    <div class="error"></div>
                 </div>
-                <button type="submit" class="btn btn-primary mt-3">Publish apartment</button>
+                <button type="submitEl" class="btn btn-primary mt-3">Publish apartment</button>
             </form>
         </div>
     </div>
