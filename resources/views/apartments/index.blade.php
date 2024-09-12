2c1a9ee62450c2255c38c04c25bd7d1e8ed51bb5
ElisabettaMirti <belmirti@gmail.com>
2024-09-12 12:38:09 +0200

    fixed index

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -57,77 +57,8 @@
         @endforeach
     </div>
 
-
-
-
-
-
-
-
-            <tbody>
-                @foreach ($apartments as $apartment)
-                    @if (Auth::user()->id == $apartment['user_id'])
-                        <tr>
-                            <td>
-                                <a class="d-flex apartment-info align-items-center p-2" href="{{ route('apartments.show',$apartment) }}">
-                                    <img class="mx-3" src="{{ asset('storage/' . $apartment->image) }}" alt="apartment image">
-                                    <div>
-                                        <strong>{{ $apartment->name}}</strong>
-                                        <p class="m-0">{{ $apartment->address}}</p>
-                                    </div>
-                                </a>
-                            </td>
-                                @if ( $apartment->visible == 1)
-                                    <td class="text-center align-middle"><div class="green circle mx-auto"></div>
-                                    </td>
-                                @else
-                                    <td class="text-center align-middle"><div class="grey circle mx-auto"></td>
-                                @endif
-                            <td class="text-center align-middle">{{ $apartment->n_room}}</td>
-                            <td class="text-center align-middle">{{ $apartment->n_bed}}</td>
-                            <td class="text-center align-middle">{{ $apartment->n_bath}}</td>
-                            <td class="text-center align-middle">
-                                <div class="d-flex justify-content-center align-items-center">
-                                    <a href=""><i class="fa-regular fa-message mx-2"></i></a>
-                                    <a href=""><i class="fa-solid fa-chart-column mx-2"></i></a>
-                                    <div id="profile" class="dropdown ">
-                                        <a id="navbarDropdown" class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i class="fa-solid fa-gear mx-2"></i>
-                                        </a>
-
-                                        <div class="dropdown-menu dropdown-menu-end">
-                                            <a class="dropdown-item" href="{{ route('apartments.edit', $apartment) }}">
-                                                Modifica
-                                            </a>
-                                            <form action="{{route('apartments.destroy', $apartment)}}" class="form-delete" method="POST" data-apartment-name="{{$apartment->name}}">
-                                                @method("delete")
-                                                @csrf
-                                                <button class="dropdown-item text-danger" href="">
-                                                    Elimina
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                    @endif
-                @endforeach
-            </tbody>
-
-        </table>
-
-        <div class="row my-4">
-            <div class="col-12 d-flex flex-row-reverse">
-                <a class="btn btn-success ms-2" href="{{route('apartments.create')}}">Aggiungi un nuovo appartamento</a>
-                <a class="btn btn-warning" href="{{route('apartments.trashed')}}">Cestino</a>
-            </div>
-        </div>
-
-
-</div>
-
 @endsection
+
 
 @section('scripts')
     @vite('resources/js/delete-confirmation.js')
