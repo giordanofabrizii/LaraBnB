8eb01a27024baf71388263867deaad20313e91eb
MirkoValle <mirko.valle20@gmail.com>
2024-09-12 18:02:52 +0200

    Added api service for apartments

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function index(){
+
+        $apartments = Apartment::with("services")->paginate(10);
+
+        return response()->json(
+        [
+            'success' => true,
+            'results' => $apartments
+        ]);
+
+    }
+
+
+    public function show(Apartment $apartment){
+
+        $apartment->loadMissing("services");
+
+        return response()->json([
+            'success' => true,
+            'results' => $apartment
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApartmentController as ApartmentController;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/statistics', [ApartmentController::class, 'getViewsData']);
+
+Route::get('/apartments', [ApiController::class, 'index'])->name('api.apartment.index');
+Route::get('apartments/{apartment}', [ApiController::class, 'show'])->name('api.apartment.show');
